8ac328e3b2aaa266d87376d921ac9c7363c06572
Jason Mann <on2valhalla@gmail.com>
2014-04-02 23:39:32 -0400

    decision stump classifier function

--- adaboost.r
+++ adaboost.r
@@ -7,22 +7,37 @@
 # c_hat <- agg_class(X, alpha, allPars)
 
 
+# Weak Learner function (Decision Stump)
+# 
 # X: a matrix, columns are training vectors x1..xn
 # w: vector containing the weights for each training vector x
 # y: vector containing class labels for each training vector x
 # return: a list which contains the parameters specifying the resulting 
 # 	classifier, here a triplet (j, theta, m) specifying the decision stump
 # 
-train <- function(X, w, y) {
+train_decision_stump <- function(X, w, y) {
 
 	return (j, theta, m)
 }
 
+
+# Applies a Weak learner to the data and gives class labels
+# 
 # X: the training data (columns are vectors of the data)
 # pars: the result of a training function, here a triplet specifying decision
-# 	stumps
-label <- function(X, pars) {
+# 	stumps (j, theta, m)
+classify_decision_stump <- function(X, pars) {
+	n <- length(X[,1])
+	j <- pars[1]
+	theta <- pars[2]
+	m <- pars[3]
+	# initialize class labels
+	y_prime <- vector(mode='numeric',length=n)
 
+	for(i in 1:n) {
+		y_prime[i] <- ifelse(X[j,i] > theta, m, -m)
+	}
+	return y_prime
 }
 
 
---
+++ randoms.r
@@ -0,0 +1,4 @@
+X <- as.matrix(read.table('uspsdata.txt'))
+y <- scan('uspscl.txt')
+
+length(X[,1])
